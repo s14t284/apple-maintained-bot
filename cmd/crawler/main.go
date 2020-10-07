cdc351b722e5b456afa9c03426b4ea96f2be813d
Ryuya Ikeda <rikeda71@gmail.com>
2020-10-08 01:37:34 +0900

    refactor crawler

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -3,13 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/labstack/gommon/log"
 	"github.com/s14t284/apple-maitained-bot/config"
-	"github.com/s14t284/apple-maitained-bot/domain/model"
 	"github.com/s14t284/apple-maitained-bot/infrastructure"
-	"github.com/s14t284/apple-maitained-bot/infrastructure/database"
-	"github.com/s14t284/apple-maitained-bot/parser"
 	"github.com/s14t284/apple-maitained-bot/usecase"
 	"github.com/s14t284/apple-maitained-bot/usecase/repository"
 	"github.com/s14t284/apple-maitained-bot/utils"
@@ -19,42 +15,6 @@ const rootURL = "https://www.apple.com"
 const shopListEndPoint = "/jp/shop/refurbished/"
 
 var products = []string{"mac", "ipad", "watch"}
-
-func searchMacFromExists(newMac *model.Mac, macInteractor repository.MacRepository) (flag bool, err error) {
-	mac, _ := macInteractor.FindByURL(newMac.URL)
-	if mac != nil {
-		mac.IsSold = false
-		log.Infof("Unsold: %s", mac.URL)
-		err = macInteractor.UpdateMac(mac)
-		flag = true
-		return
-	}
-	return
-}
-
-func searchIPadFromExists(newIPad *model.IPad, ipadInteractor repository.IPadRepository) (flag bool, err error) {
-	ipad, _ := ipadInteractor.FindByURL(newIPad.URL)
-	if ipad != nil {
-		ipad.IsSold = false
-		log.Infof("Unsold: %s", ipad.URL)
-		err = ipadInteractor.UpdateIPad(ipad)
-		flag = true
-		return
-	}
-	return
-}
-
-func searchWatchFromExists(newWatch *model.Watch, watchInteractor repository.WatchRepository) (flag bool, err error) {
-	watch, _ := watchInteractor.FindByURL(newWatch.URL)
-	if watch != nil {
-		watch.IsSold = false
-		log.Infof("Unsold: %s", watch.URL)
-		err = watchInteractor.UpdateWatch(watch)
-		flag = true
-		return
-	}
-	return
-}
 
 func main() {
 	config, err := config.LoadConfig()
@@ -67,78 +27,21 @@ func main() {
 		log.Errorf(err.Error())
 		panic(err)
 	}
-	var macInteractor repository.MacRepository
-	macInteractor = &usecase.MacInteractor{MacRepository: database.MacRepositoryImpl{SQLClient: psqlClient}}
-	var ipadInteractor repository.IPadRepository
-	ipadInteractor = &usecase.IPadInteractor{IPadRepository: database.IPadRepositoryImpl{SQLClient: psqlClient}}
-	var watchInteractor repository.WatchRepository
-	watchInteractor = &usecase.WatchInteractor{WatchRepository: database.WatchRepositoryImpl{SQLClient: psqlClient}}
+
+	var macInteractor repository.MacRepository = usecase.NewMacInteractor(psqlClient)
+	var ipadInteractor repository.IPadRepository = usecase.NewIPadInteractor(psqlClient)
+	var watchInteractor repository.WatchRepository = usecase.NewWatchInteractor(psqlClient)
 
 	for _, product := range products {
-		doc, err := utils.GetGoQueryObject(rootURL + shopListEndPoint + product)
-		if err != nil {
-			panic(err)
-		}
-
-		// 一旦、全て売れていることにする
-		// スクレイピングの際に売れ残っている判定を実施する
 		switch product {
 		case products[0]:
-			macInteractor.UpdateAllSoldTemporary()
+			utils.CrawlMacPage(rootURL, shopListEndPoint, macInteractor)
 		case products[1]:
-			ipadInteractor.UpdateAllSoldTemporary()
+			utils.CrawlIPadPage(rootURL, shopListEndPoint, ipadInteractor)
 		case products[2]:
-			watchInteractor.UpdateAllSoldTemporary()
+			utils.CrawlWatchPage(rootURL, shopListEndPoint, watchInteractor)
 		default:
 			panic(fmt.Errorf("invalid path parameter: %s", product))
 		}
-		doc.Find("div .refurbished-category-grid-no-js > ul > li").Each(func(_ int, s *goquery.Selection) {
-			title := s.Find("h3 > a").Text()
-			amount := s.Find("div,.as-currentprice,.producttile-currentprice").Text()
-			href, _ := s.Find("a").Attr("href")
-			var pageParser parser.IParser
-			pageParser = &parser.Parser{Title: title, AmountStr: amount, DetailURL: rootURL + href}
-			switch product {
-			case products[0]:
-				mac, _ := pageParser.ParseMacPage()
-				flag, err := searchMacFromExists(mac, macInteractor)
-				if err != nil {
-					log.Errorf(err.Error())
-				}
-				if !flag {
-					err := macInteractor.AddMac(mac)
-					if err != nil {
-						log.Errorf(err.Error())
-					}
-				}
-			case products[1]:
-				ipad, _ := pageParser.ParseIPadPage()
-				flag, err := searchIPadFromExists(ipad, ipadInteractor)
-				if err != nil {
-					log.Errorf(err.Error())
-				}
-				if !flag {
-					err := ipadInteractor.AddIPad(ipad)
-					if err != nil {
-						log.Errorf(err.Error())
-					}
-				}
-			case products[2]:
-				watch, _ := pageParser.ParseWatchPage()
-				flag, err := searchWatchFromExists(watch, watchInteractor)
-				if err != nil {
-					log.Errorf(err.Error())
-				}
-				if !flag {
-					err := watchInteractor.AddWatch(watch)
-					if err != nil {
-						log.Errorf(err.Error())
-					}
-				}
-			default:
-				err = fmt.Errorf("invalid product parameter: %s", product)
-				panic(err)
-			}
-		})
 	}
 }
--- utils/crawl.go
+++ utils/crawl.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/labstack/gommon/log"
+	"github.com/s14t284/apple-maitained-bot/parser"
+	"github.com/s14t284/apple-maitained-bot/usecase/repository"
 )
 
 const userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
@@ -35,4 +37,128 @@ func GetGoQueryObject(requestURL string) (*goquery.Document, error) {
 		return nil, err
 	}
 	return doc, nil
+}
+
+// CrawlMacPage macに関する整備済み品ページをクローリング
+func CrawlMacPage(rootURL string, endPoint string, mr repository.MacRepository) {
+	doc, err := GetGoQueryObject(rootURL + endPoint + "mac")
+	if err != nil {
+		panic(err)
+	}
+
+	// 一旦、全て売れていることにする
+	// クローリングの際に売れ残っている判定を実施する
+	mr.UpdateAllSoldTemporary()
+
+	titles, amounts, hrefs := scrapeMaintainedPage(doc)
+	for i := 0; i < len(titles); i++ {
+		// タイトルなどから情報をパース
+		var pageParser parser.IParser = &parser.Parser{Title: titles[i], AmountStr: amounts[i], DetailURL: rootURL + hrefs[i]}
+		mac, _ := pageParser.ParseMacPage()
+		// すでにDBに格納されているか確認
+		macInDB, err := mr.FindByURL(mac.URL)
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+		// 格納されている場合、まだ売れていないように戻す
+		// 格納されていない場合、情報を追加
+		if macInDB != nil {
+			mac.IsSold = false
+			log.Infof("Unsold: %s", mac.URL)
+			err = mr.UpdateMac(mac)
+		} else {
+			err = mr.AddMac(mac)
+		}
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+	}
+}
+
+// CrawlIPadPage ipadに関する整備済み品ページをクローリング
+func CrawlIPadPage(rootURL string, endPoint string, ir repository.IPadRepository) {
+	doc, err := GetGoQueryObject(rootURL + endPoint + "ipad")
+	if err != nil {
+		panic(err)
+	}
+
+	// 一旦、全て売れていることにする
+	// クローリングの際に売れ残っている判定を実施する
+	ir.UpdateAllSoldTemporary()
+
+	titles, amounts, hrefs := scrapeMaintainedPage(doc)
+	for i := 0; i < len(titles); i++ {
+		// タイトルなどから情報をパース
+		var pageParser parser.IParser = &parser.Parser{Title: titles[i], AmountStr: amounts[i], DetailURL: rootURL + hrefs[i]}
+		ipad, _ := pageParser.ParseIPadPage()
+		// すでにDBに格納されているか確認
+		ipadInDB, err := ir.FindByURL(ipad.URL)
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+		// 格納されている場合、まだ売れていないように戻す
+		// 格納されていない場合、情報を追加
+		if ipadInDB != nil {
+			ipad.IsSold = false
+			log.Infof("Unsold: %s", ipad.URL)
+			err = ir.UpdateIPad(ipad)
+		} else {
+			err = ir.AddIPad(ipad)
+		}
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+	}
+}
+
+// CrawlWatchPage watchに関する整備済み品ページをクローリング
+func CrawlWatchPage(rootURL string, endPoint string, wr repository.WatchRepository) {
+	doc, err := GetGoQueryObject(rootURL + endPoint + "watch")
+	if err != nil {
+		panic(err)
+	}
+
+	// 一旦、全て売れていることにする
+	// クローリングの際に売れ残っている判定を実施する
+	wr.UpdateAllSoldTemporary()
+
+	titles, amounts, hrefs := scrapeMaintainedPage(doc)
+	for i := 0; i < len(titles); i++ {
+		// タイトルなどから情報をパース
+		var pageParser parser.IParser = &parser.Parser{Title: titles[i], AmountStr: amounts[i], DetailURL: rootURL + hrefs[i]}
+		watch, _ := pageParser.ParseWatchPage()
+		// すでにDBに格納されているか確認
+		watchInDB, err := wr.FindByURL(watch.URL)
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+		// 格納されている場合、まだ売れていないように戻す
+		// 格納されていない場合、情報を追加
+		if watchInDB != nil {
+			watch.IsSold = false
+			log.Infof("Unsold: %s", watch.URL)
+			err = wr.UpdateWatch(watch)
+		} else {
+			err = wr.AddWatch(watch)
+		}
+		if err != nil {
+			log.Errorf(err.Error())
+		}
+	}
+}
+
+// ScrapeMaintainedPage 整備済み品ページから共通の情報を取得
+func scrapeMaintainedPage(doc *goquery.Document) (titles []string, amounts []string, hrefs []string) {
+	// 一件ずつスクレイピング
+	doc.Find("div .refurbished-category-grid-no-js > ul > li").Each(func(_ int, s *goquery.Selection) {
+		// タイトル、金額、詳細ページへのURL
+		title := s.Find("h3 > a").Text()
+		amount := s.Find("div,.as-currentprice,.producttile-currentprice").Text()
+		href, _ := s.Find("a").Attr("href")
+		// 格納
+		titles = append(titles, title)
+		amounts = append(amounts, amount)
+		hrefs = append(hrefs, href)
+	})
+	return
 }
