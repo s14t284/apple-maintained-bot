db00c3a998b9578874f1eb555ad671a7139e9665
Ryuya Ikeda <rikeda71@gmail.com>
2020-10-13 22:11:12 +0900

    Remove print from timezone

--- utils/timezone.go
+++ utils/timezone.go
@@ -1,14 +1,11 @@
 package utils
 
 import (
-	"fmt"
 	"time"
 )
 
 // GetReleaseYearAndMonth 商品が売られ始めた年月を表現するための時間を返却
 func GetReleaseYearAndMonth(year int, month int) time.Time {
 	timeZone, _ := time.LoadLocation("Asia/Tokyo")
-	fmt.Println(year)
-	fmt.Println(time.Month(month))
 	return time.Date(year, time.Month(month), 1, 9, 0, 0, 0, timeZone)
 }
