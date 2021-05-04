ab8a5a38fa03c296d92172f31b4a3a7d344e9d63
Ryuya Ikeda <rikeda71@gmail.com>
2021-05-04 11:57:56 +0900

    implement api controller

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/robfig/cron/v3"
 
 	"github.com/s14t284/apple-maitained-bot/config"
-	"github.com/s14t284/apple-maitained-bot/handler"
+	"github.com/s14t284/apple-maitained-bot/controller"
 	"github.com/s14t284/apple-maitained-bot/infrastructure"
 	"github.com/s14t284/apple-maitained-bot/infrastructure/database"
 	"github.com/s14t284/apple-maitained-bot/infrastructure/web"
@@ -24,6 +24,13 @@ import (
 
 var activeConWg sync.WaitGroup
 var numberOfActive = 0
+var listener *net.TCPListener
+
+type clients struct {
+	srv         *http.Server
+	crawlerCron *cron.Cron
+	crawler     usecase.CrawlerUseCase
+}
 
 func execCrawl(f func() error) {
 	err := f()
@@ -32,6 +39,7 @@ func execCrawl(f func() error) {
 	}
 }
 
+// TODO: 将来的に cron ではなく、API呼び出しでcrawlingするように修正
 func getCronConfig(crawler usecase.CrawlerUseCase) (*cron.Cron, error) {
 	c := cron.New()
 
@@ -46,68 +54,15 @@ func getCronConfig(crawler usecase.CrawlerUseCase) (*cron.Cron, error) {
 	return c, err
 }
 
-func main() {
-	// 設定読み込み
-	conf, err := config.LoadConfig()
-	if err != nil {
-		log.Errorf("cannot load config: %s", err.Error())
-		panic(err)
-	}
-	// scraper
-	scraper, err := service.NewScrapeServiceImpl()
-	if err != nil {
-		err = fmt.Errorf("failed to initialize scraper [error][%w]", err)
-		log.Error(err)
-		panic(err)
-	}
-	// pps
-	ppr, err := web.NewPageParseRepositoryImpl()
-	if err != nil {
-		err = fmt.Errorf("failed to initialize ppr [error][%w]", err)
-		log.Error(err)
-		panic(err)
-	}
-	pps, err := parse.NewPageParseServiceImpl(ppr)
-	if err != nil {
-		err = fmt.Errorf("failed to initialize pps [error][%w]", err)
-		log.Error(err)
-		panic(err)
-	}
-	// slack notifier
-	notifier, err := infrastructure.NewSlackNotifyRepositoryImpl(conf.SlackNotifyConfig)
-	if err != nil {
-		err = fmt.Errorf("failed to initialize slack notifier [error][%w]", err)
-		log.Error(err)
-		panic(err)
-	}
-	// DB接続
-	psqlClient, err := database.PsqlNewClientImpl(conf.PsqlConfig)
-	if err != nil {
-		log.Error(err)
-		panic(err)
-	}
-	macService := service.NewMacServiceImpl(database.MacRepositoryImpl{SQLClient: psqlClient})
-	ipadService := service.NewIPadServiceImpl(database.IPadRepositoryImpl{SQLClient: psqlClient})
-	watchService := service.NewWatchServiceImpl(database.WatchRepositoryImpl{SQLClient: psqlClient})
-	// crawler
-	crawler, err := usecase.NewCrawlerInteractor(macService, ipadService, watchService, pps, scraper, notifier)
-	if err != nil {
-		log.Error(err)
-	}
+func getAPIServer(gc *controller.GetController) (*http.Server, error) {
+	mux := http.NewServeMux()
+	// GetControllerを登録
+	mux.HandleFunc("/", gc.HealthCheck)
+	mux.HandleFunc("/mac", gc.GetMacHandler)
+	mux.HandleFunc("/ipad", gc.GetIPadHandler)
+	mux.HandleFunc("/watch", gc.GetWatchHandler)
 
-	// cronを設定
-	c, err := getCronConfig(crawler)
-	if err != nil {
-		log.Fatal(err)
-		panic(err)
-	}
-	c.Start()
-	// 一度クローリングを実行
-	go execCrawl(crawler.CrawlMacPage)
-	go execCrawl(crawler.CrawlIPadPage)
-	go execCrawl(crawler.CrawlWatchPage)
-
-	// http server 用の設定とserve
+	// http srv 用の設定とserve
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080" // set default port
@@ -116,26 +71,14 @@ func main() {
 	signal.Notify(ch, os.Interrupt)
 	laddr, err := net.ResolveTCPAddr("tcp", ":"+port)
 	if err != nil {
-		log.Fatal(err)
-		panic(err)
+		return nil, err
 	}
-	listener, err := net.ListenTCP("tcp", laddr)
+	listener, err = net.ListenTCP("tcp", laddr)
 	if err != nil {
-		log.Fatal(err)
-		panic(err)
+		return nil, err
 	}
-	// 仮のエンドポイント
-	// TODO: 修正する
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		_, err := w.Write([]byte("{\"message\": \"ok\"}"))
-		if err != nil {
-			log.Error(err)
-		}
-	})
-	http.HandleFunc("/mac", handler.GetMacHandler(macService))
-	http.HandleFunc("/ipad", handler.GetIPadHandler(ipadService))
-	http.HandleFunc("/watch", handler.GetWatchHandler(watchService))
 
+	// ctrl + c を受け取った時に終了できるように設定
 	go func() {
 		sig := <-ch
 		log.Info(sig)
@@ -145,21 +88,113 @@ func main() {
 		}
 	}()
 
+	srv := &http.Server{
+		Addr:    port,
+		Handler: mux,
+		ConnState: func(conn net.Conn, state http.ConnState) {
+			if state == http.StateActive {
+				activeConWg.Add(1)
+				numberOfActive++
+			} else if state == http.StateIdle || state == http.StateHijacked {
+				activeConWg.Done()
+				numberOfActive--
+			}
+			log.Info("Number of active connection: " + strconv.Itoa(numberOfActive) + "\n")
+		}}
+	return srv, nil
+}
+
+func initializeClients(conf *config.Config) (*clients, error) {
+	// pps
+	ppr, err := web.NewPageParseRepositoryImpl()
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize ppr [error][%w]", err)
+	}
+	pps, err := parse.NewPageParseServiceImpl(ppr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize pps [error][%w]", err)
+	}
+	// scraper
+	scraper, err := service.NewScrapeServiceImpl()
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize scraper [error][%w]", err)
+	}
+	// slack notifier
+	notifier, err := infrastructure.NewSlackNotifyRepositoryImpl(conf.SlackNotifyConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize slack notifier [error][%w]", err)
+	}
+	// DB接続
+	psqlClient, err := database.PsqlNewClientImpl(conf.PsqlConfig)
+	if err != nil {
+		return nil, err
+	}
+	// service, usecase
+	macService := service.NewMacServiceImpl(database.MacRepositoryImpl{SQLClient: psqlClient})
+	macUseCase, err := usecase.NewMacInteractor(macService)
+	if err != nil {
+		return nil, err
+	}
+	ipadService := service.NewIPadServiceImpl(database.IPadRepositoryImpl{SQLClient: psqlClient})
+	ipadUseCase, err := usecase.NewIPadInteractor(ipadService)
+	if err != nil {
+		return nil, err
+	}
+	watchService := service.NewWatchServiceImpl(database.WatchRepositoryImpl{SQLClient: psqlClient})
+	watchUseCase, err := usecase.NewWatchInteractor(watchService)
+	if err != nil {
+		return nil, err
+	}
+	getController, err := controller.NewGetController(macUseCase, ipadUseCase, watchUseCase)
+	if err != nil {
+		return nil, err
+	}
+	server, err := getAPIServer(getController)
+	if err != nil {
+		return nil, err
+	}
+
+	// crawler
+	crawler, err := usecase.NewCrawlerInteractor(macService, ipadService, watchService, pps, scraper, notifier)
+	if err != nil {
+		return nil, err
+	}
+	// cronを設定
+	crawlerCron, err := getCronConfig(crawler)
+	if err != nil {
+		return nil, err
+	}
+
+	return &clients{
+		srv:         server,
+		crawlerCron: crawlerCron,
+		crawler:     crawler,
+	}, nil
+}
+
+func main() {
+	// 設定読み込み
+	conf, err := config.LoadConfig()
+	if err != nil {
+		log.Errorf("cannot load config: %s", err.Error())
+		panic(err)
+	}
+	clients, err := initializeClients(conf)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+	log.Info("Start Crawling")
+	clients.crawlerCron.Start()
+	// 一度クローリングを実行
+	go execCrawl(clients.crawler.CrawlMacPage)
+	go execCrawl(clients.crawler.CrawlIPadPage)
+	go execCrawl(clients.crawler.CrawlWatchPage)
+
 	log.Info("Run Server...")
-	srv := &http.Server{Handler: nil, ConnState: func(conn net.Conn, state http.ConnState) {
-		if state == http.StateActive {
-			activeConWg.Add(1)
-			numberOfActive++
-		} else if state == http.StateIdle || state == http.StateHijacked {
-			activeConWg.Done()
-			numberOfActive--
-		}
-		log.Info("Number of active connection: " + strconv.Itoa(numberOfActive) + "\n")
-	}}
-	err = srv.Serve(listener)
+	err = clients.srv.Serve(listener)
 	if err != nil {
 		log.Fatal("serve error: " + err.Error())
-		panic(err)
 	}
 	defer activeConWg.Wait()
 }
---
+++ controller/get_controller.go
@@ -0,0 +1,313 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/labstack/gommon/log"
+
+	"github.com/s14t284/apple-maitained-bot/domain/model"
+	"github.com/s14t284/apple-maitained-bot/usecase"
+)
+
+// GetController データの取得に関するhandlerを提供するController
+type GetController struct {
+	mu usecase.MacUseCase
+	iu usecase.IPadUseCase
+	wu usecase.WatchUseCase
+}
+
+// NewGetController GetControllerを初期化して返す
+func NewGetController(mu usecase.MacUseCase, iu usecase.IPadUseCase, wu usecase.WatchUseCase) (*GetController, error) {
+	if mu == nil {
+		return nil, fmt.Errorf("mac usecase must not be nil")
+	}
+	if iu == nil {
+		return nil, fmt.Errorf("ipad usecase must not be nil")
+	}
+	if wu == nil {
+		return nil, fmt.Errorf("watch usecase must not be nil")
+	}
+	return &GetController{
+		mu: mu,
+		iu: iu,
+		wu: wu,
+	}, nil
+}
+
+// GetMacHandler macのGetリクエストの API Handler
+func (gc *GetController) GetMacHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	req := model.MacRequestParam{}
+
+	// parse request parameters
+	err := r.ParseForm()
+	if err != nil {
+		log.Errorf("failed to parse request parameter [error][%w]", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	for k, v := range r.Form {
+		switch k {
+		case "name":
+			req.Name = GetMacName(v[0])
+		case "color":
+			req.Color = GetColor(v[0])
+		case "is_sold":
+			req.IsSold = GetIsSold(v[0])
+		case "max_amount":
+			req.MaxAmount = GetAmount(v[0])
+		case "min_amount":
+			req.MinAmount = GetAmount(v[0])
+		case "max_inch":
+			req.MaxInch = GetInch(v[0])
+		case "min_inch":
+			req.MinInch = GetInch(v[0])
+		case "max_storage":
+			req.MaxStorage = GetStorage(v[0])
+		case "min_storage":
+			req.MinStorage = GetStorage(v[0])
+		case "max_memory":
+			req.MaxMemory = GetMemory(v[0])
+		case "min_memory":
+			req.MinMemory = GetMemory(v[0])
+		}
+	}
+
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	macs, err := gc.mu.GetMacs(req)
+	if err != nil {
+		log.Errorf("failed to find mac information from db [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	obj, err := json.Marshal(macs)
+	if err != nil {
+		log.Errorf("failed to parse mac information to json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, err = w.Write(obj)
+	if err != nil {
+		log.Errorf("failed to write json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// GetIPadHandler ipadのGetリクエストの API Handler
+func (gc *GetController) GetIPadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	req := model.IPadRequestParam{}
+	// parse request parameters
+	err := r.ParseForm()
+	if err != nil {
+		log.Errorf("failed to parse resquest parameter [error][%w]", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	for k, v := range r.Form {
+		switch k {
+		case "name":
+			req.Name = GetIPadName(v[0])
+		case "color":
+			req.Color = GetColor(v[0])
+		case "is_sold":
+			req.IsSold = GetIsSold(v[0])
+		case "max_amount":
+			req.MaxAmount = GetAmount(v[0])
+		case "min_amount":
+			req.MinAmount = GetAmount(v[0])
+		case "max_inch":
+			req.MaxInch = GetInch(v[0])
+		case "min_inch":
+			req.MinInch = GetInch(v[0])
+		case "max_storage":
+			req.MaxStorage = GetStorage(v[0])
+		case "min_storage":
+			req.MinStorage = GetStorage(v[0])
+		}
+	}
+
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	ipads, err := gc.iu.GetIPads(req)
+	if err != nil {
+		log.Errorf("failed to find ipad information from db [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	obj, err := json.Marshal(ipads)
+	if err != nil {
+		log.Errorf("failed to parse ipad information to json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, err = w.Write(obj)
+	if err != nil {
+		log.Errorf("failed to write json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// GetWatchHandler apple watchのGetリクエストの API Handler
+func (gc *GetController) GetWatchHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	req := model.WatchRequestParam{}
+	// parse request parameters
+	err := r.ParseForm()
+	if err != nil {
+		log.Errorf("failed to parse request parameter [error][%w]", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	for k, v := range r.Form {
+		switch k {
+		case "name":
+			req.Name = GetWatchName(v[0])
+		case "color":
+			req.Color = GetColor(v[0])
+		case "is_sold":
+			req.IsSold = GetIsSold(v[0])
+		case "max_amount":
+			req.MaxAmount = GetAmount(v[0])
+		case "min_amount":
+			req.MinAmount = GetAmount(v[0])
+		case "max_inch":
+			req.MaxInch = GetInch(v[0])
+		case "min_inch":
+			req.MinInch = GetInch(v[0])
+		case "max_storage":
+			req.MaxStorage = GetStorage(v[0])
+		case "min_storage":
+			req.MinStorage = GetStorage(v[0])
+		}
+	}
+
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	watches, err := gc.wu.GetWatches(req)
+	if err != nil {
+		log.Errorf("failed to find apple watch information from db [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	obj, err := json.Marshal(watches)
+	if err != nil {
+		log.Errorf("failed to parse apple watch information to json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	_, err = w.Write(obj)
+	if err != nil {
+		log.Errorf("failed to write json [error][%w]", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+// HealthCheck ヘルスチェック用
+func (gc *GetController) HealthCheck(w http.ResponseWriter, req *http.Request) {
+	_, err := w.Write([]byte("{\"message\": \"ok\"}"))
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+// -------------------------
+// 以下は各メソッドで使う共通処理
+// -------------------------
+
+// GetColor リクエストパラメータから色を取得
+func GetColor(str string) string {
+	switch str {
+	case "ゴールド", "シルバー", "スペースグレイ":
+		return str
+	default:
+		return ""
+	}
+}
+
+// GetInch リクエストパラメータからインチ数を取得
+func GetInch(inch string) float64 {
+	val, err := strconv.ParseFloat(inch, 64)
+	if err != nil {
+		log.Warnf("failed to parse inch size from request parameter [error][%w]", err)
+		return 0.0
+	}
+	return val
+}
+
+// GetAmount リクエストパラメータから金額を取得
+func GetAmount(amount string) int {
+	val, err := strconv.ParseInt(amount, 10, 64)
+	if err != nil {
+		log.Warnf("failed to parse amount from request parameter [error][%w]", err)
+		return 0
+	}
+	return int(val)
+}
+
+// GetStorage リクエストパラメータから金額を取得
+func GetStorage(storage string) int {
+	val, err := strconv.ParseInt(storage, 10, 64)
+	if err != nil {
+		log.Warnf("failed to storage from request parameter [error][%w]", err)
+		return 0
+	}
+	return int(val)
+}
+
+// GetIsSold リクエストパラメータから売り切れているかを取得
+// isSoldで絞り込みをするのはrepositoryで行うので、文字列をそのまま返す
+func GetIsSold(str string) string {
+	return str
+}
+
+// GetMemory リクエストパラメータからメモリを取得
+func GetMemory(storage string) int {
+	val, err := strconv.ParseInt(storage, 10, 64)
+	if err != nil {
+		log.Warnf("failed to memory from request parameter [error][%w]", err)
+		return 0
+	}
+	return int(val)
+}
+
+// GetMacName リクエストパラメータからMacの名前を取得
+func GetMacName(str string) string {
+	switch strings.ToLower(str) {
+	case "pro":
+		return "MacBook Pro"
+	case "air":
+		return "MacBook Air"
+	case "macbook":
+		return "MacBook"
+	case "mini":
+		return "Mac mini"
+	case "macpro":
+		return "Mac Pro"
+	default:
+		return ""
+	}
+}
+
+// GetIPadName リクエストパラメータからIPadの名前を取得
+func GetIPadName(str string) string {
+	switch strings.ToLower(str) {
+	case "mini":
+		return "IPad mini"
+	case "pro":
+		return "IPad Pro"
+	case "unmarked":
+		return "IPad"
+	default:
+		return ""
+	}
+}
+
+// GetWatchName リクエストパラメータからapple watchの名前を取得
+func GetWatchName(str string) string {
+	return strings.ToTitle(str)
+}
